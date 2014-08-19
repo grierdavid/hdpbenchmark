bd9553beef6da9de2de32556b401abd9b53312b0
David Grier <david.grier@rackspace.com>
2014-08-19 14:39:57 +0000

    itterating over issues related to attaching CBS

--- build_servers_cbs.py
+++ build_servers_cbs.py
@@ -1,80 +1,149 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 
 import os
 import sys
 import pyrax
 import time
+import string
 
 '''
 Build Servers with attached CBS and CloudNetworks
 '''
 
-cred_file = os.path.expanduser('~/.rackspace_cloud_credentials')
+cred_file = os.path.expanduser('.ipython/profile_cbdteam/conf/.rackspace_cloud_credentials')
 pyrax.set_credential_file(cred_file)
 
-cs = pyrax.cloudservers
-cbs = pyrax.cloud_blockstorage
-cnw = pyrax.cloud_networks
+my_region = 'IAD'
+
+cs = pyrax.connect_to_cloudservers(region=my_region)
+cbs = pyrax.connect_to_cloud_blockstorage(region=my_region)
+cnw = pyrax.connect_to_cloud_networks(region=my_region)
+
+#| onmetal-compute1 | OnMetal Compute v1      | 32768     | 32   | 0         |         | 20    | 10000.0     | N/A       |
+#| onmetal-io1      | OnMetal I/O v1          | 131072    | 32   | 3200      |         | 40    | 10000.0     | N/A       |
+#| onmetal-memory1  | OnMetal Memory v1       | 524288    | 32   | 0         |         | 24    | 10000.0     | N/A       |
+#| performance1-1   | 1 GB Performance        | 1024      | 20   | 0         |         | 1     | 200.0       | N/A       |
+#| performance1-2   | 2 GB Performance        | 2048      | 40   | 20        |         | 2     | 400.0       | N/A       |
+#| performance1-4   | 4 GB Performance        | 4096      | 40   | 40        |         | 4     | 800.0       | N/A       |
+#| performance1-8   | 8 GB Performance        | 8192      | 40   | 80        |         | 8     | 1600.0      | N/A       |
+#| performance2-120 | 120 GB Performance      | 122880    | 40   | 1200      |         | 32    | 10000.0     | N/A       |
+#| performance2-15  | 15 GB Performance       | 15360     | 40   | 150       |         | 4     | 1250.0      | N/A       |
+#| performance2-30  | 30 GB Performance       | 30720     | 40   | 300       |         | 8     | 2500.0      | N/A       |
+#| performance2-60  | 60 GB Performance       | 61440     | 40   | 600       |         | 16    | 5000.0      | N/A       |
+#| performance2-90  | 90 GB Performance
 
 
 #Needs to be fqdn and based on a domain associated with this account
+clust_prefix='dgbm02-'
+dn_name=clust_prefix  + 'datanode'
+mn_name=clust_prefix  + 'masternode'
 pvt_nets = []
 cbs_ids = []
-srv_ids = []
+cbs_ids_dict = {}
+mn_ids = []
+dn_ids = []
 imgname = 'CentOS 6.5'
-size = 512
-new_network_name = "hadoop_net"
-new_network_cidr = "192.168.0.0/24"
-
+mn_flavor = 'performance2-15'
+dn_flavor = 'performance2-15'
+new_network_name = clust_prefix + 'hadoop_net'
+new_network_cidr = "192.168.5.0/24"
+datanodes = 3
+datanode = 1
+masternodes=2
+masternode=1
+vols_per_datanode=3
+cbs_vol_type="SATA"
+pubkey='dgmuse'
 
 new_net = cnw.create(new_network_name, cidr=new_network_cidr)
 print "New network:", new_net
 networks = new_net.get_server_networks(public=True, private=True)
 
-for i in range(1, 4):
-    cbsname = 'mycbs' + str(i)
-    sata_vol = cbs.create(name=cbsname, size=500, volume_type="SATA")
-    pyrax.utils.wait_until(sata_vol,"status","available", interval=1)
-    cbs_ids.append(sata_vol.id)
-    print "vol %s: %s" % (cbsname, sata_vol)
+vol_end_range = vols_per_datanode + 1
+while (datanode <= datanodes):
+	for i in range(1, vol_end_range):
+	    my_dn_name  = dn_name + str(datanode)
+	    cbsname = my_dn_name + 'vol' + str(i)
+	    vol = cbs.create(name=cbsname, size=500, volume_type=cbs_vol_type)
+	    pyrax.utils.wait_until(vol,"status","available", interval=1)
+            my_dn_name = my_dn_name + str(i)
+	    cbs_ids_dict[my_dn_name] = vol.id
+            print "name: %s dict: %s vol_id: %s" % (my_dn_name, cbs_ids_dict[my_dn_name], vol.id) 
+	    print "vol %s: %s" % (cbsname, vol)
+        datanode = datanode + 1
 
+datanode=1
 myimage = [img for img in cs.images.list()
                 if imgname in img.name][0]
-myflavor = [flavor for flavor in cs.flavors.list()
-                if flavor.ram == size][0]
+#myflavor = [flavor for flavor in cs.flavors.list()
+#                if flavor.ram == size][0]
 
-for i in range(1, 4):
-    server_name='web' + str(i)
-    server = cs.servers.create(server_name, myimage.id, myflavor.id, nics=networks)
-    print "Name:", server.name
-    print "ID:", server.id
-    srv_ids.append(server.id)
-    adminpass = server.adminPass
-    print "Admin Password:", adminpass
-    print "Waiting for Network config.."
-    while not cs.servers.get(server.id).networks:
-      time.sleep(1)
-    pvt_net = str(cs.servers.get(server.id).networks['private'][0])
-    new_net = str(cs.servers.get(server.id).networks[new_network_name][0])
-    pub_net = [ ip for ip in cs.servers.get(server).networks['public']
-                if len( ip.split(".") ) == 4 ]
+while (datanode <= datanodes):
+	for i in range(1, 4):
+	    server_name=dn_name + str(i)
+	    server = cs.servers.create(server_name, myimage.id, dn_flavor, key_name=pubkey, nics=networks)
+	    print "Name:", server.name
+	    print "ID:", server.id
+	    dn_ids.append(server.id)
+	    adminpass = server.adminPass
+	    print "Admin Password:", adminpass
+	    print "Waiting for Network config.."
+	    while not cs.servers.get(server.id).networks:
+	      time.sleep(1)
+	    pvt_net = str(cs.servers.get(server.id).networks['private'][0])
+	    new_net = str(cs.servers.get(server.id).networks[new_network_name][0])
+	    pub_net = [ ip for ip in cs.servers.get(server).networks['public']
+			if len( ip.split(".") ) == 4 ]
 
-    pvt_nets.append(pvt_net)
-    print "Public Networks: %s" % str(pub_net[0])
-    print "Private Networks: %s" % pvt_net
-    print "Isolated Networks: %s" % new_net
+	    pvt_nets.append(pvt_net)
+	    print "Public Networks: %s" % str(pub_net[0])
+	    print "Private Networks: %s" % pvt_net
+	    print "Isolated Networks: %s" % new_net
+	    datanode = datanode + 1
+
+while (masternode <= masternodes):
+	    server_name=mn_name + str(masternode)
+	    server = cs.servers.create(server_name, myimage.id, mn_flavor, key_name=pubkey, nics=networks)
+	    print "Name:", server.name
+	    print "ID:", server.id
+	    mn_ids.append(server.id)
+	    adminpass = server.adminPass
+	    print "Admin Password:", adminpass
+	    print "Waiting for Network config.."
+	    while not cs.servers.get(server.id).networks:
+	      time.sleep(1)
+	    pvt_net = str(cs.servers.get(server.id).networks['private'][0])
+	    new_net = str(cs.servers.get(server.id).networks[new_network_name][0])
+	    pub_net = [ ip for ip in cs.servers.get(server).networks['public']
+			if len( ip.split(".") ) == 4 ]
+
+	    pvt_nets.append(pvt_net)
+	    print "Public Networks: %s" % str(pub_net[0])
+	    print "Private Networks: %s" % pvt_net
+	    print "Isolated Networks: %s" % new_net
+            masternode = masternode + 1
 
 
-for i in srv_ids:
+#Broken Logic Needs to associate correct 3 cbs vols with datanodes
+for i in dn_ids:
+    cbs_vol_arr = list(string.ascii_lowercase)
+    frst_place = 5 
     mysrv = cs.servers.get(i)
-    pyrax.utils.wait_until(server, "status", "ACTIVE", attempts=0,
+    pyrax.utils.wait_until(mysrv, "status", "ACTIVE", attempts=0,
                 verbose=True)
-    for id in cbs_ids:
-      vol = cbs.get(id)
+    vol_end_range = vols_per_datanode + 1
+    for n in range(1, vol_end_range):
+      myvol= mysrv.name + str(n)
+      vol = cbs.get(cbs_ids_dict[myvol]) 
       print "attaching vol: %s to srv: %s" % ( vol, mysrv )
-      vol.attach_to_instance(mysrv, mountpoint="/dev/xvdb")
-      pyrax.utils.wait_until(vol, "status", "in-use", interval=3, attempts=0,
-        verbose=True)
-      print "Volume attachments:", vol.attachments
-      cbs_ids.pop(0)
-      break
+      vol.attach_to_instance(mysrv, '/dev/hvd' +  str(cbs_vol_arr[frst_place + n]))
+
+#    for id in cbs_ids:
+#      vol = cbs.get(id)
+#      print "attaching vol: %s to srv: %s" % ( vol, mysrv )
+#      vol.attach_to_instance(mysrv, '/dev/zvd' + cbs_vol_arr.pop())
+#      pyrax.utils.wait_until(vol, "status", "in-use", interval=3, attempts=0,
+#        verbose=True)
+#      print "Volume attachments:", vol.attachments
+#      cbs_ids.pop()
+#      break
